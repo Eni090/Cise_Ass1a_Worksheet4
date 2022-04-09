4cb073d88154269af4774e0adb5d4711be53d71a
Eni090 <89555783+Eni090@users.noreply.github.com>
2022-04-10 11:28:46 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import Home from './pages/Home';
-import SEPractice from './pages/SE-Practice';
+import SEPractice from './pages/SE-Practices';
 import SubmitArticle from './pages/Submit-Article';
 import NotFoundPage from './pages/404';
+import { Route, Routes, NavLink, BrowserRouter as Router, Navigate } from 'react-router-dom';
 
 const App = () => {
 	return (
@@ -12,16 +12,26 @@ const App = () => {
 				<h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
 				<ul className="header">
 					<li>
-						<a href="/">Home</a>
+						<NavLink exact to="/">
+							Home
+						</NavLink>
 					</li>
 					<li>
-						<a href="/SEPractice">Select the Practice</a>
+						<NavLink to="/SEPractice">Select the Practice</NavLink>
 					</li>
 					<li>
-						<a href="/SubmitArticle">Submit an Article</a>
+						<NavLink to="/SubmitArticle">Submit an Article</NavLink>
 					</li>
 				</ul>
 				<div className="content" />
+				<Routes>
+					<Route path="/404" element={<NotFoundPage />} exact />
+					<Route path="*" element={<Navigate replace to="/404" />} />
+
+					<Route exact path="/" element={<Home />} />
+					<Route path="/SEPractice" element={<SEPractice />} />
+					<Route path="/SubmitArticle" element={<SubmitArticle />} />
+				</Routes>
 			</div>
 		</Router>
 	);
